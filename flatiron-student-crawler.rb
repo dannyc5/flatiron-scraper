b98915da9421f47829678497c8f438fb4dc06838
Scott Luptowski <scottluptowski@gmail.com>
2013-10-06 16:36:12 -0400

    incomplete

--- flatiron-student-crawler.rb
+++ flatiron-student-crawler.rb
@@ -26,33 +26,18 @@ def crawl_page(student_page)
   #All paths should have a url.css() call in them to work properly
 
   #get name, image, quote
-  name = url.css('.ib_main_header').children[0].text
-  image = url.css('.student_pic').first.attributes["src"].value
-  quote = url.css('.textwidget h3')[0].children.text
+  name = url.css('.ib_main_header').children[0].text || "name"
+  image = url.css('.student_pic').first.attributes["src"].value || "image"
+  quote = url.css('.textwidget h3')[0].children.text.strip || "quote"
 
   # Try the following line, subbing out the "[-3]" for other elements of the array
   # to get different information
-  cities =  url.css('h3')[-3].parent.parent.children[5].children.map { |city| city.text.strip  }.reject(&:empty?)
+  cities =  url.css('h3')[-3].parent.parent.children[5].children.map { |city| city.text.strip  }.reject(&:empty?) || "cities"
   # get biographies
-  biography = url.css('h3')[1].parent.parent.children[5].text.strip
-  personal_projects = url.css('h3')[-4].parent.parent.children[5].text.strip
-  favorite_websites = url.css('h3')[8].parent.parent.children[5].children.map { |site| site.text.gsub("-","").strip  }.reject(&:empty?)
-  favorite_websites = Hash[*favorite_websites.flatten]
-
-  #get favorite cities
-  services_div_contents = url.css('.services')
-  # cities = []
-  
-  # services_div_contents.each do |service_div|
-  #   headers = service_div.css('h3')
-  #   headers.each do |tag|
-  #     if tag.children.text == "Favorite Cities"
-  #       service_div.css('p a').each do |parsed_tag|
-  #         cities << parsed_tag.children.text
-  #       end
-  #     end
-  #   end
-  # end
+  biography = url.css('h3')[1].parent.parent.children[5].text.strip || "bio"
+  personal_projects = url.css('h3')[-4].parent.parent.children[5].text.strip || "projects"
+  #favorite_websites = url.css('h3')[8].parent.parent.children[5].children.map { |site| site.text.gsub("-","").strip  }.reject(&:empty?)
+  #favorite_websites = Hash[*favorite_websites.flatten]
 
   #get social media links
   social_media_array = []
@@ -65,15 +50,25 @@ def crawl_page(student_page)
     social_media_array << instance.attributes["href"].value 
   end
    
-  social_media_array.uniq!
+  social_media_array = social_media_array.uniq || "social media"
   ### end social, needs to be able to remove one of the github instances, leaving for now to get larger functionality going
 
 
   #RETURN VALUE, FOR TESTING PURPOSES
-  [name,image,quote,cities,social_media_array, biography, favorite_websites].inspect
+  [name,image,quote,cities,social_media_array, biography].inspect
 
 end
 #binding.pry
 
+final_text = ""
 #For Testing purposes, run this function on Vivian and see what comes back
 puts crawl_page(urlmap[0]).inspect
+puts crawl_page(urlmap[2]).inspect
+puts crawl_page(urlmap[3]).inspect
+puts crawl_page(urlmap[5]).inspect
+
+# test = urlmap.map do |site|
+#   crawl_page(site)
+# end
+
+# puts test.join.inspect
